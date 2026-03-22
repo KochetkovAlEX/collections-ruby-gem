c11a5a53471aed27d4b028a7f1ee16591f6818c9
Kochetkov Aleksey <leshakochetcov@gmail.com>
2026-03-22 22:39:22 +0300

    Create new methods for Linked List

--- main.rb
+++ main.rb
@@ -2,20 +2,18 @@
 require 'collections'
 
 ### LinkedList check ###
-# l1 = Collections::LinkedList.new
-# l1.append(12)
-# l1.append(13)
-# l1.append(14)
-# l1.append(15)
-# l1.append(16)
-# l1.append(17)
-# l1.show
-# l1.delete(2)
-# p "-"*10
-# l1.show
-# p "-"*10
-# l1.insert_into(2, 130)
-# l1.show
-# p "-"*10
-# l1.insert_into(3, 220)
-# l1.show
+l1 = Collections::LinkedList.new(11)
+l1.append(12)
+l1.append(13)
+l1.append(14)
+l1.append(15)
+l1.append(16)
+l1.append(17)
+p l1.to_a
+p "#"*15
+p l1.delete_at(2)
+p l1.to_a
+p "#"*15
+p l1.find(15)
+p "#"*15
+p l1.at(0)
