aebd1d8d6e73150c6b97ac3d1f314a1174c9ec34
qqgriggs <anazaro@sfedu.ru>
2026-03-25 21:31:13 +0300

    Update collections.rb

--- lib/collections.rb
+++ lib/collections.rb
@@ -4,6 +4,10 @@ require_relative "collections/version"
 require_relative "collections/node"
 require_relative "collections/linked_list"
 
+# Binary Tree
+require_relative "binary_tree/node"
+require_relative "binary_tree/tree"
+
 
 module Collections
   class Error < StandardError; end
